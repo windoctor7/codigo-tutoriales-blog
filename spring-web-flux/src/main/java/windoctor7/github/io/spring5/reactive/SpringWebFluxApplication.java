f268450e1082a919239082164bd75140af632f07
ascariromopedraza <molder.itp@gmail.com>
2017-04-21 00:17:02 -0500

    ejemplo usando spring boot 2 para programacion reactiva

---
+++ spring-web-flux/src/main/java/windoctor7/github/io/spring5/reactive/SpringWebFluxApplication.java
@@ -0,0 +1,12 @@
+package windoctor7.github.io.spring5.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxApplication.class, args);
+	}
+}
